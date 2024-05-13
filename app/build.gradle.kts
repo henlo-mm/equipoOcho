85a6e05ab4db280e4e6576dfba0d9e91086722a1
DianaDev <micolta.diana@correounivalle.edu.co>
2024-05-13 15:35:20 -0500

    se modifico el diseño de nueva cita

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.biometric:biometric:1.2.0-alpha05")
     implementation("com.airbnb.android:lottie:6.4.0")
+    implementation("androidx.databinding:library:3.2.0-alpha11")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("io.github.florent37:shapeofview:1.4.7")
 }
